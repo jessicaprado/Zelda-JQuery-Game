12a7f97eee9686bba308ac1570424ee7d296c0e1
Jessica Prado <jessica.marie.prado@gmail.comgit config --global user.name Jessica>
2016-10-23 20:09:15 -0400

    sunday progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,22 +1,44 @@
 $(document).ready(function(){
 
-	$("button").on("click", function(){
-		$(this).html("#yourcharacter");
-	});
+	characterSelected = false;
+	enemySelected = false;
 
 
 
 
+//Choosing character and moving unselected to enemies
+$(".characters").on("click", "button", function(){		
+	$(this).detach(".characters").appendTo("#myCharacter");
+	$("#myCharacter").siblings().appendTo("#enemies")
+	characterSelected = true;
+	console.log(characterSelected);
 
 
+$(".characters").on("click", "button", function() {
+	if(characterSelected = true) {
+		$(this).detach().appendTo("#defender");
+		enemySelected = true;
+		console.log(enemySelected);		
+	};
 
-
-
-
-
-
-
-
-
+$("#attack").on("click", function (){
+	if((characterSelected) = true && (enemySelected = true)) {
+		console.log("awesome");
+	}
+})
+});
 
 });
+
+
+	
+
+	
+	
+//end of script tag. DO NOT TOUCH
+});
+
+
+
+	
+	
